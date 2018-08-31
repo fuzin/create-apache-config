11cc226e1eb0bdc3d81cba2085abebbce1f62c09
Bojan Kovacec <bojan.kovacec@gmail.com>
2018-08-31 21:26:40 +0200

    First working version.

--- create-apache-project.sh
+++ create-apache-project.sh
@@ -21,8 +21,11 @@ VIRTUAL_HOST_FILENAME=`printf $NR_OF_FILES-$1.conf`
 VIRTUAL_HOST_PATH=/etc/apache2/sites-available/$VIRTUAL_HOST_FILENAME
 echo "$VIRTUAL_HOST_PATH created"
 
-
 # TODO: repplace with specific apache version file
 cp /etc/apache2/sites-available/000-template $VIRTUAL_HOST_PATH
-sed -i 's/$HOSTNAME/$1/g' $VIRTUAL_HOST_PATH
 
+# replace $HOSTNAME in template @TODO: template "engine"
+CMD='s/$HOSTNAME/'
+CMD+="$1/g";
+
+sed -i $CMD $VIRTUAL_HOST_PATH
